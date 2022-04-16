717f60fc49852006657c274ece71ffaf63c08eb0
kozimbek1101 <101723936+kozimbek1101@users.noreply.github.com>
2022-04-16 14:21:07 +0500

    create js file

---
+++ index.js
@@ -0,0 +1,676 @@
+// 11-mavzu
+
+// 1-masala
+
+// let n=prompt("ixtiyoriy son kirirting");
+
+// if (n>0){
+//     n=++n;
+//     console.log(`Natija:${n}`);
+// }
+// else{
+//     console.log(`Son manfiy:${n}`);
+// }
+
+// 2-mavzu
+
+// let n=prompt("ixtiyoriy son kirirting");
+
+// if (n>0){
+//     n=++n;
+//     console.log(`Natija:${n}`);
+// }
+// else{
+//     n=n-2;
+//     console.log(`Son manfiy:${n}`);
+// }
+
+// 3-masala
+
+// let n=prompt("ixtiyoriy son kirirting");
+
+// if (n>0){
+//     n=++n;
+//     console.log(`Natija:${n}`);
+// }else
+// if (n==0){
+//     n=10;
+//     console.log(`Son 0 ga teng Natija:${n}`);
+// }
+// else{
+//     n=n-2;
+//     console.log(`Son manfiy:${n}`);
+// }
+
+// 4-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let c=prompt("C=");
+// let s=0;
+
+// if (a>0){
+//     console.log(`${a} Musbat`);
+//     s=s+1;
+// }
+// else{
+//     console.log(`${a} Manfiy`)
+    
+// }
+
+// if (b>0){
+//     console.log(`${b} Musbat`);
+//     s=s+1;
+// }
+// else{
+//     console.log(`${b} Manfiy`)
+// }
+
+// if (c>0){
+//     console.log(`${c} Musbat`);
+//     s=s+1;
+// }
+// else{
+//     console.log(`${c} Manfiy`)
+// }
+// console.log(`${s} ta musbat son bor`)
+
+// 5-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let c=prompt("C=");
+// let s=0;
+// let m=0;
+
+// if (a>0){
+//     console.log(`${a} Musbat`);
+//     s=s+1;
+// }
+// else{
+//     console.log(`${a} Manfiy`)
+//     m=m+1;
+// }
+
+// if (b>0){
+//     console.log(`${b} Musbat`);
+//     s=s+1;
+// }
+// else{
+//     console.log(`${b} Manfiy`)
+//     m=m+1;
+// }
+
+// if (c>0){
+//     console.log(`${c} Musbat`);
+//     s=s+1;
+// }
+// else{
+//     console.log(`${c} Manfiy`)
+//     m=m+1;
+// }
+// console.log(`${s} ta musbat son bor`)
+// console.log(`${m} ta manfiy son bor`)
+
+// 6-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+
+// if (a>b){
+// console.log(`Katta son:${a}`);
+// }
+// else{
+//     console.log(`Katta son:${b}`);   
+// }
+
+// 7-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+
+// if (a<b){
+// console.log(`1-son kichik:${a}`);
+// }
+// else{
+//     console.log(`2-son kichik:${b}`);   
+// }
+
+// 8-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+
+// if (a>b){
+// console.log(`Katta son:${a}`);
+// console.log(`kichik son:${b}`);
+
+// }
+// else{
+//     console.log(`Katta son:${b}`);
+//     console.log(`Kichik son:${a}`);   
+// }
+
+// 9-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+
+// if (a>b){
+//     let d=(a-b)+1;
+//     b=b*1+d;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+// }
+// else{
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);    
+// }
+
+// 10-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let d=a;
+
+// if (a!=b){
+    
+//     a=a*1+b*1;
+//     b=b*1+d*1;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+// }
+// else{
+//     a=0;
+//     b=0;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+// }
+
+// 11-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let d=a;
+
+// if (a!=b){
+// if (a>b){
+//     a=a;
+//     b=a;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+// }
+// else {
+//     a=b;
+//     b=b;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+// }  
+    
+// }
+// else{
+//     a=0;
+//     b=0;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+// }
+
+// 12-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let c=prompt("C=");
+
+// if((a<b)&&(a<c)){
+//     console.log(`Eng kichik son:${a}`);
+// } 
+// else
+// {if((b<a)&&(b<c)){
+//     console.log(`Eng kichik son:${b}`);
+// } else{
+//     if((c<a)&&(c<b)){
+//         console.log(`Eng kichik son:${c}`);
+//     } 
+// }
+// }
+
+// 13-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let c=prompt("C=");
+// let max=Math.max(a,b,c);
+// let min=Math.min(a,b,c)
+// if((a>min)&&(a<max)){
+//     console.log(`O'rtacha son:${a}`);
+// } 
+// else
+// {if((b>min)&&(b<max)){
+//     console.log(`O'rtacha son:${b}`);
+// } else{
+//     if((c>min)&&(c<max)){
+//         console.log(`O'rtacha son:${c}`);
+//     } 
+// }
+// }
+
+// 14-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let c=prompt("C=");
+
+// if ((a<b)&&(a<c)){
+//     console.log(`${a} eng kichik son`);
+//     }
+//     else{
+//         if ((b<a)&&(b<c)) {
+//             console.log(`${b} eng kichik son`);
+//         }else
+//     if  ((c<a)&&(c<b)){
+//         console.log(`${c} eng kichik son`);  
+//     }
+//     }
+//     if ((a>b)&&(a>c)){
+//         console.log(`${a} eng katta son`);
+//         }
+//         else{
+//             if ((b>a)&&(b>c)) {
+//                 console.log(`${b} eng katta son`);
+//             }else
+//         if  ((c>a)&&(c>b)){
+//             console.log(`${c} eng katta son`);  
+//         }
+//         }
+//  15-masala
+
+// let a=prompt(`A=`);
+// let b=prompt(`B=`);
+// let c=prompt(`C=`);
+
+// let a1=a*1+b*1;
+// let a2=a*1+c*1;
+// let a4=b*1+c*1;
+// let a5=c*1+a*1;
+// let max=Math.max(a1,a2,a4,a5);
+
+// if (max==a1){
+//     console.log(`Eng katta yig'indi A+B=${a1}`)
+// }else{
+//     if (max==a2){
+//         console.log(`Eng katta yig'indi A+C=${a2}`)
+//     }else{
+//         if (max==a4){
+//             console.log(`Eng katta yig'indi B+C=${a4}`)
+//         } else
+//         {
+//             if (max==a5){
+//                 console.log(`Eng katta yig'indi A+C=${a5}`)
+//             }
+//         }       
+//     } 
+// }
+
+// 16-masala
+
+// let a=prompt(`A=`);
+// let b=prompt(`B=`);
+// let c=prompt(`C=`);
+
+// if ((a<b)&&(b<c)) {
+//     a=2*a;
+//     b=2*b;
+//     c=2*c;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+//     console.log(`C=${c}`);
+// }
+// else{
+//     a=-a;
+//     b=-b;
+//     c=-c;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+//     console.log(`C=${c}`);
+// }
+
+// 17-masala
+
+// let a=prompt(`A=`);
+// let b=prompt(`B=`);
+// let c=prompt(`C=`);
+
+// if (((a<b)&&(b<c))||((a>b)&&(b>c))) {
+//     a=2*a;
+//     b=2*b;
+//     c=2*c;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+//     console.log(`C=${c}`);
+// }
+// else{
+//     a=-a;
+//     b=-b;
+//     c=-c;
+//     console.log(`A=${a}`);
+//     console.log(`B=${b}`);
+//     console.log(`C=${c}`);
+// }
+
+// 18-masala
+// let a=prompt(`A=`);
+// let b=prompt(`B=`);
+// let c=prompt(`C=`);
+
+// if (a==b){
+//     console.log(`Teng bo'lmagan son C=${c} tartib raqam:3`)
+// }
+// else{
+//     if (a==c){
+//         console.log(`Teng bo'lmagan son B=${b} tartib raqam:2`)
+//     }
+//     else{
+//         if (b==c){
+//             console.log(`Teng bo'lmagan son A=${a} tartib raqam:1`)
+//         }
+//     }   
+// }
+
+// 19-masala
+
+// let a=prompt(`A=`);
+// let b=prompt(`B=`);
+// let c=prompt(`C=`);
+// let d=prompt(`D=`);
+
+// if (a==b&&a==d){
+// console.log(`Teng bo'magan son C=${c}`)
+// }
+// else{
+//     if (a==b&&a==c){
+//         console.log(`Teng bo'magan son D=${d}`);
+//         }
+//         else{
+//             if (a==c&&a==d){
+//                 console.log(`Teng bo'magan son B=${b}`);
+//                 }
+//                 else{
+//                     if (b==c&&b==d){
+//                         console.log(`Teng bo'magan son A=${a}`);
+//                         }
+//                 }
+//         }
+// }
+ 
+// 20-masala
+
+// let a=prompt("A=");
+// let b=prompt("B=");
+// let c=prompt("C=");
+
+// let ab=Math.abs(a-b);
+// let ac=Math.abs(a-c);
+
+// if (ac<ab){
+//     console.log(`Eng yaqin nuqta C=${c} Masofa:${ac}`);
+// }else {
+//     if (ac>ab){
+//         console.log(`Eng yaqin nuqta C=${b} Masofa:${ab}`);
+//     }else{
+//         if (ac==ab){
+//             console.log(`Ikkalasi ham eng yaqin nuqta!!`);
+//         }   
+//     }
+// }
+
+// 21-masala
+
+// let x=prompt(`X=`);
+// let y=prompt(`Y=`);
+
+// if (x==0&&y==0){
+//     console.log(0);
+//  } else{
+//         if (x==0&&y!=0){
+//         console.log(2);
+//     }else{
+//         if (x!==0&&y==0){
+// console.log(1);
+//         }else{
+//             console.log(3)
+//         }
+//     }
+// }
+
+// 22-masala
+
+// let x=prompt(`X=`);
+// let y=prompt(`Y=`);
+
+// if (x>0&&y>0){
+//     console.log(`Nuqta I chorakda joylashgan!!`);
+//  } else{
+//         if (x<0&&y>0){
+//         console.log(`Nuqta II chorakda joylashgan!!`);
+//     }else{
+//         if (x<0&&y<0){
+// console.log(`Nuqta III chorakda joylashgan!!`);
+//         }else{
+//             console.log(`Nuqta IV chorakda joylashgan!!`)
+//         }
+//     }
+// }
+
+// 23-masala
+
+// let x1=prompt(`X1=`);
+// let y1=prompt(`Y1=`);
+// let x2=prompt(`X2=`);
+// let y2=prompt(`Y2=`);
+// let x3=prompt(`X3=`);
+// let y3=prompt(`Y3=`);
+
+// console.log(`Tortinchi nuqta kordinatasi (${x3};${y1})`);
+
+// 24-masala
+
+// let x=prompt(`X=`);
+
+// if (x>0){
+//     console.log(`Natija:${2*Math.sin(x)}`)
+// }else{
+//     if (x<=0){
+//         console.log(`Natija:${x-6}`);
+//     }
+// }
+
+// 25-masala
+
+// let x=prompt(`X=`);
+
+// if (x<-2||x>2){
+//     console.log(`Natija:${2*x}`)
+// }else{
+    
+//         console.log(`Natija:${-3*x}`);
+
+// }
+
+// 26-masala
+
+// let x=prompt(`X=`);
+
+// if (x<=0){
+//     console.log(`Natija:${-x}`)
+// }else{
+//     if (x<0&&x<2){
+//         console.log(`Natija:${x*x}`);
+// }else
+//     if (x>=2){
+//         console.log(`Natija:${4}`);
+//     }
+// }
+
+// 27-masala
+
+// let x=prompt(`X=`);
+
+// if (x>=0&&(x%2)==0&&(x%2)<1){
+//     console.log(1);
+// }else{
+//     if (x>=1&&(x%2)>0&&(x%2)<=1){
+//         console.log(-1);
+//     } 
+// }
+
+// 28-masala
+
+// let x=prompt(`Yilni kirirting`)
+//  if (x>=400&&(x%400)==0){
+//      console.log(`Kiritilgan yilda 366 kun bor`);
+//  }else{
+//      if (x<400&&(x%4)==0){
+//         console.log(`Kiritilgan yilda 366 kun bor`);    
+//      }
+//      else{
+//          console.log(`Kiritilgan yilda 365 kun bor`);
+//      }
+//  }
+
+// 29-masala
+
+// let a=prompt(`Butun son kiriting:`);
+
+// if (a>0&&a%2==0){
+//     console.log(`Berilgan ${a} son musbat va juft`);
+// }else{
+//     if (a>0&&a%2>0){
+//         console.log(`Berilgan ${a} son musbat va toq`);
+// }else{
+//     if (a<0&&a%2==0){
+//         console.log(`Berilgan ${a} son Manfiy va juft`);
+//     }else{
+//         if (a>0&&a%2<0){
+//             console.log(`Berilgan ${a} son Manfiy va toq`);
+//         }else{
+        
+//                 console.log(`Berilgan son ${a} ga teng `);
+    
+//         }
+//     }
+// }  
+// }
+
+// 30-masala
+
+// let a=prompt(`1-999 oraliqda son kiriting:`);
+
+// if (a/10<1&&a%2==0){
+//     console.log(`Berilgan ${a} son bir xonali va juft`);
+// }else{
+//     if (a/10<1&&(a%2>0||a==1)){
+//         console.log(`Berilgan ${a} son bir xonali va toq`);
+// }else{
+//     if (a/10>=1&&a%2==0&&a<100){
+//         console.log(`Berilgan ${a} son ikki xonali va juft`);
+//     }else{
+//         if (a/10>=1&&a%2>0&&a<100){
+//             console.log(`Berilgan ${a} son ikki xonali va toq`);
+//         }else{
+        
+//             if (a/100>=1&&a%2==0){
+//                 console.log(`Berilgan ${a} son uch xonali va juft`);
+//             } else
+//             if (a/100>=1&&a%2>0){
+//                 console.log(`Berilgan ${a} son uch xonali va toq`);
+//             }   
+    
+//         }
+//     }
+// }  
+// }
+
+
+// 13-mavzu
+// 1-masala
+
+// let a=prompt((`Hafta kuni tartib raqami`));
+// a=a*1;
+// switch(a){
+// case 1: console.log(`${a}-Dushanba`);break;
+// case 2: console.log(`${a}-Seshanba`);break;
+// case 3: console.log(`${a}-Chorshanba`);break;
+// case 4: console.log(`${a}-Payshanba`);break;
+// case 5: console.log(`${a}-Juma`);break;
+// case 6: console.log(`${a}-Shanba`);break;
+// case 7: console.log(`${a}-Yakshanba`);break;
+// default:console.log(`Bunday hafta kuni yo'q!!!`);
+// }
+
+// 2-masala
+
+// let a=prompt((`Bahoni kiriting`));
+// a=a*1;
+// switch(a){
+// case 1: console.log(`${a}-Yomon`);break;
+// case 2: console.log(`${a}-Qoniqarsiz`);break;
+// case 3: console.log(`${a}-Qoniqarli`);break;
+// case 4: console.log(`${a}-Yaxshi`);break;
+// case 5: console.log(`${a}-A'lo`);break;
+
+// default:console.log(`XATOOOO!!!`);
+// }
+
+// 3-masala
+
+// let a=prompt((`Oy tartib raqami`));
+// a=a*1;
+// switch(a){
+// case 1: 
+// case 2: console.log(`QISH`);break;
+// case 3: 
+// case 4: 
+// case 5: 
+// case 6: console.log(`BAHOR`);break;
+// case 7:
+// case 8:   console.log(`YOZ`);break;
+// case 9: 
+// case 10: 
+// case 11:console.log(`KUZ`);break;
+// case 12:console.log(`QISH`);break;
+// default:console.log(`Bunday oy yo'q!!!`);
+// }
+
+// 4-masala
+
+// let a=prompt((`Oy tartib raqami`));
+// a=a*1;
+// switch(a){
+// case 1:console.log(`Yanvar 31 kun`); break;
+// case 2:console.log(`Fevral 28-29 kun`); break;
+// case 3:console.log(`Mart 31 kun`); break; 
+// case 4:console.log(`Aprel 30 kun`); break; 
+// case 5:console.log(`May 31 kun`); break; 
+// case 6:console.log(`Iyun 30 kun`); break;
+// case 7:console.log(`Iyul 31 kun`); break;
+// case 8:console.log(`Avgust 31 kun`); break;
+// case 9:console.log(`Sentabr 30 kun`); break; 
+// case 10:console.log(`Oktabr 31 kun`); break; 
+// case 11:console.log(`Noyabr 30 kun`); break;
+// case 12:console.log(`Dekabr 31 kun`); break;
+// default:console.log(`Bunday oy yo'q!!!`);
+// }
+
+// 5-masala
+let a=prompt(`A=`)*1;
+let b=prompt(`B=`)*1;
+let c=prompt(`Amal tartib raqamini kirirting(1:"+" 2:"-" 3:"/" 4:"*")`);
+c=c*1;
+switch(c){
+case 1:console.log(`Yig'indi:${a+b}`);break;
+case 2:console.log(`Ayirma:${a-b}`);break;
+case 3:console.log(`Bo'linma:${a/b}`);break;
+case 4:console.log(`Ko'paytma:${a*b}`);break;
+default:console.log(`Bunday amal yo'q`);
+}
+
